26ecca6dd8fdf3172648f94e77c929a293ecefac
Kadams35 <kacyadams35@gmail.com>
2022-03-15 19:04:58 -0400

    Factory to handle review cids

--- contracts/CollaborationFactory.sol
+++ contracts/CollaborationFactory.sol
@@ -9,9 +9,17 @@ contract CollaborationFactory {
     Counters.Counter private CollabIds;
 
     struct Researcher {
-        string cid;
+        string ipfs_cid;
+
+        uint SCIENCE_index;
+        uint peer_review_score;
+
         uint256[] sharing_ids;
         uint256[] seeking_ids;
+
+        string[] reviewAbout_cids;
+        string[] reviewFrom_cids;
+
         bool exists;
     }
 
@@ -19,7 +27,8 @@ contract CollaborationFactory {
     mapping(uint256 => address) collaborations;
 
     event CollaborationInitialized(uint256 CollabId);
-    event ParticipantAdded(string name, string institution);
+    event ParticipantAdded(string cid);
+    event ReviewSubmitted(string cid);
 
     address owner;
 
@@ -43,7 +52,7 @@ contract CollaborationFactory {
     ///         would be initialized by the data seeker
     /// @param res_1 Address of confirmed participant in the protocol who is sharing data
     /// @param res_2 Address of confirmed participant in the protocol who is seeking data
-    function _initCollab(address res_1, address res_2) public returns (address newCollab) {
+    function _initCollaboration(address res_1, address res_2) public returns (address newCollab) {
         require(participants[res_1].exists == true);
         require(participants[res_2].exists == true);
 
@@ -63,16 +72,35 @@ contract CollaborationFactory {
     /// @notice A researcher must be a confirmed participant on the protocol in
     ///         to share or seek data
     /// @param cid IPFS contentID pointing to a JSON handling researcher metadata
-    function _addRes(string memory cid) public {
+    function _addResearcher(string memory cid) public {
         require(participants[msg.sender].exists == false, "Already a researcher associated with this address");
 
         uint256[] memory arr1;
         uint256[] memory arr2;
-        participants[msg.sender] = Researcher(cid, arr1, arr2, true);
+        string[] memory arr3;
+        string[] memory arr4;
+        
+        participants[msg.sender] = Researcher(cid, 0, 0, arr1, arr2, arr3, arr4, true);
         emit ParticipantAdded(cid);
     }
 
-    
+    /// @notice Updates a researcher's peer review score and adds the review's content ID to
+    ///         the appropriate arrays for later lookup
+    /// @param  collaborationContract the collaboration the review pertains to
+    /// @param  about The researcher's address the review is about
+    /// @param  cid IPFS contentID pointing to a JSON handling the review metadata, pinned by application
+    /// @param  reviewScore calculated based on the different review fields outlined in AIO, calculated in application
+    function _submitReview(address collaborationContract, address about, string memory cid, uint reviewScore) public {
+        // check that collab contract is in correct state...
+        
+        uint currReviewCount = participants[about].reviewAbout_cids.length;
+        uint newReviewScore = ((participants[about].peer_review_score * currReviewCount) + reviewScore) / (currReviewCount + 1);
+        participants[about].peer_review_score = newReviewScore;
+
+        participants[msg.sender].reviewFrom_cids.push(cid);
+        participants[about].reviewAbout_cids.push(cid);
+    }
+
 
 
 }
